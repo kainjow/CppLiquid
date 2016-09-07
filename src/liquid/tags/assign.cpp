38678479b4cb53e8b9d714e1357d6a28c55ba6f5
Kevin Wojniak <kainjow@users.noreply.github.com>
2016-09-06 20:31:30 -0700

    Add test case

--- src/liquid/tags/assign.cpp
+++ src/liquid/tags/assign.cpp
@@ -29,7 +29,14 @@ TEST_CASE("Liquid::Assign") {
         Liquid::Template t;
         CHECK(t.parse("{% assign name = 'Steve' %}{{ name }}").render().toStdString() == "Steve");
     }
-
+    
+    SECTION("Hyphenated") {
+        Liquid::Template t;
+        Liquid::Data::Hash hash;
+        hash["a-b"] = "1";
+        Liquid::Data data(hash);
+        CHECK(t.parse("a-b:{{a-b}} {%assign a-b = 2 %}a-b:{{a-b}}").render(data) == "a-b:1 a-b:2");
+    }
 }
 
 #endif
