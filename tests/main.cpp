68f7b8dafcb384ad76f97e9d44d577ff5383584b
Kevin Wojniak <kainjow@users.noreply.github.com>
2016-11-16 22:08:09 -0800

    Add main with pause for MSVC debugging

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,2 +1,8 @@
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
+
+int main(int argc, char* const argv[]) {
+    const int result = Catch::Session().run(argc, argv);
+    // system("pause");
+    return result;
+}
